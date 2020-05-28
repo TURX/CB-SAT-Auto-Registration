612cc256db7b9aba0ada71cf4f8b00851a6d3f6f
TURX <turx2003@gmail.com>
2020-05-28 16:49:31 +0800

    1.5: reload once

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         College Board SAT Semi-Auto Registration
 // @namespace    https://github.com/TURX
-// @version      1.4
+// @version      1.5
 // @description  automatically complete several steps of SAT registration
 // @author       TURX
 // @match        https://nsat.collegeboard.org/*
@@ -13,18 +13,25 @@
     'use strict';
 
     var url = window.location.href.substr(0, window.location.href.length - window.location.search.length);
+    var timeOutReload;
+    var reloaded = false;
     console.log("[College Board SAT Semi-Auto Registration] Enabled, current URL: " + url);
 
     switch (url) {
         case "https://nsat.collegeboard.org/satweb/registration/acceptSatTermsAndConditions.action":
-            var timeOutReload;
             if (document.getElementsByClassName("s2-well-text-block").length != 0) {
                 if (document.getElementsByClassName("s2-well-text-block")[0].innerText.search("There are no available registration dates for the current test year. Please check back later to register for future tests.") != -1) {
                     console.log("[College Board SAT Semi-Auto Registration] No registration date available, will retry in 30s.");
                     timeOutReload = 30;
                     document.getElementsByClassName("s2-well-text-block")[0].innerText = "No registration date available, will retry after 30s."
                     setInterval(function() {
-                        if (timeOutReload == 0) location.reload();
+                        if (timeOutReload == 0) {
+                            if (reloaded == false) {
+                                console.log("[College Board SAT Semi-Auto Registration] Retry now.");
+                                location.reload();
+                                reloaded = true;
+                            }
+                        }
                         else timeOutReload--;
                         document.getElementsByClassName("s2-well-text-block")[0].innerText = "No registration date available, will retry after " + timeOutReload + "s."
                     }, 1000);
@@ -39,7 +46,13 @@
                     timeOutReload = 3;
                     document.getElementById("error").innerText = "[College Board SAT Semi-Auto Registration] Website error, will retry after 3s."
                     setInterval(function() {
-                        if (timeOutReload == 0) location.reload();
+                        if (timeOutReload == 0) {
+                            if (reloaded == false) {
+                                console.log("[College Board SAT Semi-Auto Registration] Retry now.");
+                                location.reload();
+                                reloaded = true;
+                            }
+                        }
                         else timeOutReload--;
                         document.getElementById("error").innerText = "[College Board SAT Semi-Auto Registration] Website error, will retry after " + timeOutReload + "s."
                     }, 1000);
@@ -77,7 +90,13 @@
             timeOutReload = 30;
             document.getElementsByClassName("cb-alert-heading")[0].getElementsByTagName("p")[0].innerText = "Website down, will retry after 30s."
             setInterval(function() {
-                if (timeOutReload == 0) location.href = "https://nsat.collegeboard.org/satweb/satHomeAction.action";
+                if (timeOutReload == 0) {
+                    if (reloaded == false) {
+                        console.log("[College Board SAT Semi-Auto Registration] Retry now.");
+                        location.href = "https://nsat.collegeboard.org/satweb/satHomeAction.action";
+                        reloaded = true;
+                    }
+                }
                 else timeOutReload--;
                 document.getElementsByClassName("cb-alert-heading")[0].getElementsByTagName("p")[0].innerText = "Website down, will retry after " + timeOutReload + "s."
             }, 1000);
